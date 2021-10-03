3f60392b2407632b982297639c4632faf17c8922
Salomelr26 <91816435+Salomelr26@users.noreply.github.com>
2021-10-03 09:32:03 -0500

    Delete AgronetApp directory

--- AgronetApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AgronetappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AgronetApp'
--- AgronetApp/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from .productos import productos
