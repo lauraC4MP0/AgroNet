3600dbaf3fd272c70fea1e818ed03c5c552584c4
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-06 10:12:09 -0500

    Add files via upload

---
+++ agronet_be/AgronetApp/models/product.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db.models.fields import NullBooleanField
+from .user import User
+
+class Product(models.Model):
+    code=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=30)
+    description=models.CharField(max_length=80)
+    price=models.IntegerField()
+    sales_unit=models.CharField(max_length=20)
+    id_seller=models.ForeignKey(User,related_name='product',null=False,on_delete=models.CASCADE)
+    amount=models.IntegerField()
+    image=models.BinaryField()
