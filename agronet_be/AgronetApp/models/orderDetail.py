ba20521ed146f84e982877e790039aacf2594599
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-06 18:24:39 -0500

    Update orderDetail.py

--- agronet_be/AgronetApp/models/orderDetail.py
+++ agronet_be/AgronetApp/models/orderDetail.py
@@ -1,8 +1,9 @@
 from django.db import models
 from .product import Product
+from .order import Order
 
 class OrderDetail(models.Model):
     id=models.AutoField(primary_key=True)
-    product_id=models.ForeignKey(Product)
+    product_id=models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     amount=models.IntegerField()
-    order_id=models.ForeignKey(Order)
+    order_id=models.ForeignKey(Order,on_delete=models.DO_NOTHING)
