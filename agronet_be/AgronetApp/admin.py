ec935430e759f547b8a86ae0d4ec507c5df731ef
Salomelr26 <91816435+Salomelr26@users.noreply.github.com>
2021-10-06 14:43:17 -0500

    Update admin.py

--- agronet_be/AgronetApp/admin.py
+++ agronet_be/AgronetApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models.departament import Departament
+from .models.city import City
+
+admin.site.register(Departament)
+admin.site.register(City)
 
 # Register your models here.
