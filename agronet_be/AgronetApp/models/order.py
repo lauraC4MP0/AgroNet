a7b9726b94720de67f595dfacb087335fc8a3c86
Anama1127 <92000105+Anama1127@users.noreply.github.com>
2021-10-06 17:18:36 -0500

    Add files via upload

---
+++ agronet_be/AgronetApp/models/order.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .user import User
+
+class order(models.Model):
+    id_order=models.AutoField(primary_key=True)
+    date_sale=models.DateTimeField(max_length=30)
+    date_delivery=models.DateTimeField(max_length=30)
+    total_order=models.IntegerField()
+    sales_unit=models.CharField(max_length=20)
+    username=models.ForeignKey(User,related_name='username',null=False,on_delete=models.CASCADE)
