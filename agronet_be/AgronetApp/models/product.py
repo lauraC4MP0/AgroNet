bfcd96148958e9d62943517644829c360687caab
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-06 20:14:44 -0500

    Update product.py

--- agronet_be/AgronetApp/models/product.py
+++ agronet_be/AgronetApp/models/product.py
@@ -3,11 +3,11 @@ from django.db.models.fields import NullBooleanField
 from .user import User
 
 class Product(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=30,null=False)
-    description=models.CharField(max_length=80, null=True)
-    price=models.IntegerField(null=False)
-    sales_unit=models.CharField(max_length=20,null=False)
-    seller=models.ForeignKey(User,related_name='product',null=False,on_delete=models.CASCADE)
-    amount=models.IntegerField(null=False)
-    image=models.BinaryField(null=True)
+    id_product=models.AutoField(primary_key=True)
+    name_product=models.CharField(max_length=30,null=False)
+    description_product=models.CharField(max_length=80, null=True)
+    price_product=models.IntegerField(null=False)
+    sales_unit_product=models.CharField(max_length=20,null=False)
+    username_fk=models.ForeignKey(User,related_name='product',null=False,on_delete=models.CASCADE)
+    amount_product=models.IntegerField(null=False)
+    image_product=models.BinaryField(null=True)
