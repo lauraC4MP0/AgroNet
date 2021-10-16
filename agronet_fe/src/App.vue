a079e584112693f40739b757ebc123c8c373f84e
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-16 08:46:58 -0500

    Add files via upload

---
+++ agronet_fe/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="app">
+    <div class="header">
+      <h1>AgroNet</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function () {},
+  methods: {},
+  created: function () {},
+};
+</script>
+
+<style
+>
+body {
+  margin: 0 0 0 0;
+}
+.header {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 10vh;
+
+  min-height: 100px;
+  background-color:seagreen;
+
+  color:white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header h1 {
+  width: 20%;
+  text-align: center;
+}
+</style>
