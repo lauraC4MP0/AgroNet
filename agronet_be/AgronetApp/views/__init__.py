9a4e830097ef6c289ea55e652bb28dbca0e91152
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-16 21:41:40 -0500

    Update __init__.py

--- agronet_be/AgronetApp/views/__init__.py
+++ agronet_be/AgronetApp/views/__init__.py
@@ -1,6 +1,6 @@
 from .productView import ProductsView
 from .productView import specificProductView
-#from .orderDetailView import OrderDetailView
+from .orderDetailView import OrderDetailView
 from .orderView import OrdersView
 from .orderView import specificOrderView
 from .userCreateView import UserCreateView
