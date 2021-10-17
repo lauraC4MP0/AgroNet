b9e2bc4528307e7356a3b363ec06a7ec862131aa
Anama1127 <92000105+Anama1127@users.noreply.github.com>
2021-10-16 22:25:15 -0500

    Add files via upload

---
+++ agronet_be/AgronetApp/views/userEditView.py
@@ -0,0 +1,23 @@
+from django.http.response import Http404, HttpResponseBadRequest
+from django.shortcuts import HttpResponse
+from rest_framework import status,views
+from rest_framework.response import Response
+from App import serializers
+from App.models import User
+from App.serializers import  userSerializer
+
+class userEditView(views.APIView):
+
+    def list(self, request, *args, **kwargs):
+        user = User.objects.all()
+        serializer=userSerializer(User,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def update(self, request,*args,**kwargs):
+        user=self.list(kwargs['username'])
+        serializer=userSerializer(user,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
