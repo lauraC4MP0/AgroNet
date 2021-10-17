9d10b3e11bec22a96d68bd119c108501699ba3a2
Anama1127 <92000105+Anama1127@users.noreply.github.com>
2021-10-17 17:24:53 -0500

    Update urls.py
    Se actualizar URLs según vistas ya disponibles

--- agronet_be/Agronetproject/urls.py
+++ agronet_be/Agronetproject/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from AgronetApp import views
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
-
-
 urlpatterns = [
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('user/', views.UserCreateView.as_view()),
+    path('user/<int:pk>/', views.UserDetailView.as_view()),
     path('orderDetail', views.OrderDetailView.as_view()),
     path('order', views.OrdersView.as_view()),
     path('products/',views.ProductsView.as_view()),
