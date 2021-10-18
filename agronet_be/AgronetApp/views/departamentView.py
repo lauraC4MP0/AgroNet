0bd2adbd9f2336dbc9fd578e388054927367d8e2
MCEscobar <89407925+MCEscobar@users.noreply.github.com>
2021-10-17 22:17:16 -0500

    Add files via upload

---
+++ agronet_be/AgronetApp/views/departamentView.py
@@ -0,0 +1,16 @@
+from rest_framework import status,views
+from rest_framework.response import Response
+from AgronetApp.models.departament import Departament
+from AgronetApp.serializers import departamentSerializer
+
+class DepartamentView(views):
+
+    def post(self, request, *args, **kwargs):
+        serializer = departamentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def list(self,request,args,*kwargs):
+        orders = Departament.objects.all()
+        serializer = departamentSerializer(Departament)
+        return Response(serializer.data,status=status.HTTP_200_OK)
