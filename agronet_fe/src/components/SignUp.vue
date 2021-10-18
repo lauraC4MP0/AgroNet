676f4cc1e08babe4a11c17224209db1a1db7dce0
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-18 17:30:13 -0500

    Update SignUp.vue

--- agronet_fe/src/components/SignUp.vue
+++ agronet_fe/src/components/SignUp.vue
@@ -1,98 +1,64 @@
 <template>
-  <div class="signup">
-    <div class="container">
-      <h2>Crear cuenta</h2>
+  <div class="header">
+    <h1>AgroNet</h1>
+    <nav>
+      <button v-if="!is_auth" v-on:click="loadLogIn">Iniciar sesión</button>
+      <button v-if="!is_auth" v-on:click="loadSignUp">Crear cuenta</button>
+    </nav>
+  </div>
+  <div class="main-component">
+    <form v-on:submit.prevent="processSignUp">
+      <h3>Crear cuenta</h3>
+      <br/>
+      <input type="text" v-model="user.usersname" placeholder="Número de documento de identidad" required />
       <br />
-      <h3>Ingrese su información, por favor:</h3>
+      <input type="text" v-model="user.name_user" placeholder="Nombres" required />
       <br />
-      <form v-on:submit.prevent="processSignUp">
-        <input
-          type="text"
-          v-model="user.usersname"
-          placeholder="Número de documento de identidad"
-          required
-        />
-        <br />
-        <input
-          type="text"
-          v-model="user.name_user"
-          placeholder="Nombres"
-          required
-        />
-        <br />
-        <input
-          type="text"
-          v-model="user.lastname_user"
-          placeholder="Apellidos"
-          required
-        />
-        <br />
-        <input
-          type="text"
-          v-model="user.address_user"
-          placeholder="Dirección"
-          required
-        />
-        <br />
-        <input
-          type="tel"
-          v-model="user.num_phone"
-          placeholder="Teléfono"
-          required
-        />
-        <br />
-        <input
-          type="password"
-          v-model="user.password"
-          placeholder="Contraseña (8 carácteres)"
-          minlength="8"
-          required
-        />
-        <br />
-        <input
-          type="email"
-          v-model="user.email"
-          placeholder="Correo electrónico"
-          required
-        />
-        <br />
-        <select class="sex">
-          <option>Sexo</option>
-          <option value="Masculino">Masculino</option>
-          <option value="Femenino">Femenino</option>
-          <option>No binario</option>
-          <option>Otro</option>
-        </select>
-        <br />
-        <select class="department">
-          <option>Departamento</option>
-        </select>
-        <br />
-        <select>
-          <option>Ciudad</option>
-        </select>
-        <br />
-        <p>¿Eres agricultor?</p>
-        <input type="radio" name="rol" checked />
-        <label>Sí, quiero mostrar mis productos</label>
-        <br />
-        <input type="radio" name="rol" />
-        <label>No, soy comerciante</label>
-        <br />
-        <input
-          type="password"
-          placeholder="Confirma tu contraseña"
-          minlength="8"
-          required
-        />
-        <br />
-        <button type="submit">Registrarse</button>
-      </form>
-    </div>
+      <input type="text" v-model="user.lastname_user" placeholder="Apellidos" required />
+      <br />
+      <input type="tel" v-model="user.num_phone" placeholder="Teléfono" required />
+      <br />
+      <input type="email" v-model="user.email" placeholder="Correo electrónico" required />
+      <br />
+      <input type="password" v-model="user.password" placeholder="Contraseña (8 carácteres)" minlength="8" required />
+      <br />
+      <input type="password" placeholder="Confirma tu contraseña" minlength="8" required />
+      <br />
+      <input type="text" v-model="user.address_user" placeholder="Dirección" required />
+      <br />
+      <select class="sex">
+        <option>Sexo</option>
+        <option value="Masculino">Masculino</option>
+        <option value="Femenino">Femenino</option>
+        <option>No binario</option>
+        <option>Otro</option>
+      </select>
+      <br />
+      <select class="department">
+        <option>Departamento</option>
+      </select>
+      <br />
+      <select>
+        <option>Ciudad</option>
+      </select>
+      <br />
+      <p>¿Eres agricultor?</p>
+      <input type="radio" name="rol" checked />
+      <label>Sí, quiero mostrar mis productos</label>
+      <br />
+      <input type="radio" name="rol" />
+      <label>No, soy comerciante</label>
+      <br />
+      <button type="submit">Registrarse</button>
+    </form>
+  </div>
+  <div class="footer">
+    <h2>Del campo a tu mesa</h2>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: "SignUp",
@@ -137,44 +103,122 @@ export default {
 </script>
 
 <style>
-.signup {
-  margin: 0;
-  padding: 0%;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+body{
+    margin: 0 0 0 0;
+    box-sizing: border-box;
+  }
 
-.container {
-  border: 3px solid seagreen;
+form{
+  width: 300px;
+  padding: 16px;
   border-radius: 10px;
-  width: 25%;
-  height: 60%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  margin:auto;
+  background-color:mediumaquamarine;
+  margin-top: 20px;
+}
+
+form h3{
+  height: 0;
+  padding: 0%;
+}
+
+form input[type="text"],form input[type="email"],form input[type="tel"], form input[type="password"],select{
+  width: 250px;
+  padding:3px 10px;
+  border:1px solid white;
+  border-radius: 3px;
+  background-color: #f6f6f6;
+  margin:8px 0;
+  display: inline-block;
+}
+
+form input[type="radio"]{
+  font-style:italic;
+}
+form button{
+  width:100%;
+  padding: 8px 16px;
+  margin-top: 28px;
+  border-radius: 5px;
+  border:1px solid #000;
+  display: block;
+  color:white;
+  background-color: #000;
+}
+
+form button:hover{
+  cursor:pointer;
+}
+
+textarea{
+  width:100%;
+  height: 100px;
+  border:1px solid #f6f6f6;
+  margin:8px 0;
+  resize: none;
+  display: block;
+}
+
+.header{
+  margin: 0%;
+  padding: 0;
+  width: 100%;
+  height:10vh;
+  min-height: 100px;
+  background-color:seagreen;
+  color:seashell;
+  display:flex;
+  justify-content: space-between;
   align-items: center;
 }
 
-.signup h2,
-h3 {
+.header h1{
+  width: 20%;
+  text-align: center;
+}
+
+.header nav{
+  height: 100%;
+  width: 20%;
+  display:flex;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 20px;
+}
+
+.header nav button{
+  color:mediumaquamarine;
+  background-color: whitesmoke;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding:10px 20px;
+}
+
+.header nav button:hover{
   color: seagreen;
+  background-color: #e5e7e9;
+  border: 1px solid #E5E7E9;
+  cursor: pointer;
 }
 
-.signup form {
-  width: 70%;
+.footer{
+  margin:0;
+  padding:0;
+  width:100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color:seagreen;
+  color: #E5E7E9;
 }
 
-.signup input {
-  height: 40px;
-  width: 100%;
-  box-sizing: border-box;
-  padding: 10px 20px;
-  margin: 5px 0;
-  border: 1px solid seagreen;
+.footer h2{
+  width: 90%;
+  height: 100%;
+  display: flex;
+  justify-content:right;
+  align-items: center;
 }
+</style>
 
 .signup button {
   width: 100%;
