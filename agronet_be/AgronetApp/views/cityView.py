20dd188f0adecbfc5132c29141437e0d6dd33f6a
MCEscobar <89407925+MCEscobar@users.noreply.github.com>
2021-10-17 22:13:38 -0500

    Add files via upload

---
+++ agronet_be/AgronetApp/views/cityView.py
@@ -0,0 +1,16 @@
+from rest_framework import status,views
+from rest_framework.response import Response
+from AgronetApp.models.city import City
+from AgronetApp.serializers import citySerializer
+
+class CityView(views):
+
+    def post(self, request, *args, **kwargs):
+        serializer = citySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def list(self,request,args,*kwargs):
+        orders = City.objects.all()
+        serializer = citySerializer(City)
+        return Response(serializer.data,status=status.HTTP_200_OK)
