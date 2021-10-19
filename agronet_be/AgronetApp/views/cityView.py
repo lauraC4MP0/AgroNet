37e62a9fca412313d7d5fb4546b551dcc48296c4
JulyBarragan <julyazmin.barragan@gmail.com>
2021-10-18 23:09:49 -0500

    ajustes vista city

--- agronet_be/AgronetApp/views/cityView.py
+++ agronet_be/AgronetApp/views/cityView.py
@@ -5,12 +5,9 @@ from AgronetApp.serializers import citySerializer
 
 class CityViews(views.APIView):
 
-    def post(self, request, *args, **kwargs):
-        serializer = citySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-    def list(self,request,args,*kwargs):
-        orders = City.objects.all()
-        serializer = citySerializer(City)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+ 
+        
+    def get(self, request):
+         ciudad = City.objects.all()              
+         serializer = citySerializer.CitySerializer(ciudad, many=True)         
+         return Response(serializer.data,status=status.HTTP_200_OK)
