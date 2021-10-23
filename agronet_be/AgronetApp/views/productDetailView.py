f27e595e862fdc34c1c946906ad3ab524770054c
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-23 14:35:24 -0500

    Create productDetailView.py

---
+++ agronet_be/AgronetApp/views/productDetailView.py
@@ -0,0 +1,10 @@
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import PasswordField, TokenObtainPairSerializer
+from agroApp.models.product import Product
+from rest_framework import generics
+from AgronetApp.serializers.productSerializer import ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
