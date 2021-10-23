d28e862f5416670fe5d640151e94bcfd62d52e72
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-23 14:41:20 -0500

    Update order.py

--- agronet_be/AgronetApp/models/order.py
+++ agronet_be/AgronetApp/models/order.py
@@ -5,8 +5,6 @@ class order(models.Model):
     id_order=models.AutoField(primary_key=True)
     date_sale=models.DateTimeField(max_length=30)
     date_delivery=models.DateTimeField(max_length=30)
-    total_order=models.IntegerField()
-    sales_unit=models.CharField(max_length=20)
     username=models.ForeignKey(User,related_name='username',null=False,on_delete=models.CASCADE)
 
     def __str__(self):
