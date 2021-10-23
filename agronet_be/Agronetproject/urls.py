3b93c1a1f398acb227f9295b4a461443ecfb15ab
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-23 14:44:12 -0500

    Update urls.py

--- agronet_be/Agronetproject/urls.py
+++ agronet_be/Agronetproject/urls.py
@@ -14,10 +14,9 @@ urlpatterns = [
     path('orderDetail/', OrderDetailView.as_view()),
     path('orderDetail/{id}', OrderDetailDetail.as_view()),
     path('order/', views.OrdersView.as_view()),
+    path('order/<int:pk>',views.OrdersDetail.as_view())
     path('product/',views.ProductCreateView.as_view()),
     path('product/<int:pk>',views.ProductDetailView.as_view()),
     path('city/',views.CityViews.as_view()),
     path('departament/',views.DepartamentView.as_view()),
-
-    
 ]
