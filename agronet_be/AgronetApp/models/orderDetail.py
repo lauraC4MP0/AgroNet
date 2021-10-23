ed1800085d3f26b0c7e07a6279e8bd6047a66d71
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-23 14:42:02 -0500

    Update orderDetail.py

--- agronet_be/AgronetApp/models/orderDetail.py
+++ agronet_be/AgronetApp/models/orderDetail.py
@@ -6,10 +6,7 @@ from django.db.models import Sum, F
 class OrderDetail(models.Model):
     id_order_detail=models.AutoField(primary_key=True)
     id_product_fk=models.ForeignKey(Product.id_product, on_delete=models.DO_NOTHING)
-    #total_price_product=models.IntegerField(null=True)
     amount_order=models.IntegerField(null=True)
-    price_unit=models.IntegerField(null=True)
-    total_price_product=models.IntegerField(null=True)
     id_order_fk=models.ForeignKey(order.id_order,on_delete=models.DO_NOTHING)  
     def __str__(self):
      return self.id_order_fk
