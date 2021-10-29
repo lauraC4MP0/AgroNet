ee8859edb469332cfe48a14c0cf3d83311c7fbc1
MCEscobar <89407925+MCEscobar@users.noreply.github.com>
2021-10-29 07:47:18 -0500

    Add files via upload

---
+++ agronet_fe/src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="home">
+        <h1>Bienvenido a AGRONET {{ username }}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Home',
+        data: function(){
+            return {
+                username: localStorage.getItem('username') || "none"
+            }
+        }
+    }
+</script>
+
+<style>
+    .home{
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .home h1{
+        font-size: 50px;
+        color: #0b5240;
+        
+    }
+</style>
