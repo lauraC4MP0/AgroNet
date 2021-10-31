aab67a20fa4b5e92c5d59a9fa27df0762f98f859
Laura Campo <86002500+lauraC4MP0@users.noreply.github.com>
2021-10-31 10:12:03 -0500

    Create updateProfile.vue

---
+++ agronet_fe/src/components/updateProfile.vue
@@ -0,0 +1,34 @@
+<template>
+  <center>
+    <div class="main-component">
+      <p>
+        Desde <b>AgroNet</b> te pedimos disculpas por las molestias presentadas,
+        puedes dirigirte
+        <a href="https://ciclo3grupo2agroclicbd.herokuapp.com/user/"
+          >a esta dirección</a
+        >
+        para editar tu perfil. <br /><br />Estamos trabajando para resolver
+        el inconveniente.
+      </p>
+    </div>
+  </center>
+</template>
+
+<style >
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+p {
+  background-color: mediumaquamarine;
+  width: 400px;
+  border-radius: 10px;
+  font-size: 20px;
+  margin:auto;
+    align-content: center;
+    align-items: center;
+  padding:16px ;
+  margin-top: 20px;
+}
+</style>
